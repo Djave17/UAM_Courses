b28c495c4c18c748f4e62b6375c2bf2d7aba68aa
Djave17 <dsancheza@uamv.edu.ni>
2024-10-31 08:36:38 -0600

    Inicialización de proyecto

---
+++ AppGestionUAM/AppGestionUAM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppGestionUAM
+//
+//  Created by David Sanchez on 31/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
