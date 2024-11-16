2fd386c3813d949465432e5d717adc3a4e9c6560
Djave17 <dsancheza@uamv.edu.ni>
2024-11-16 17:13:45 -0600

    Crear model, APIClientRestful, prueba de lista de cursos con datos DUMMY

--- AppGestionUAM/AppGestionUAM/Views/Courses/Cells/CourseCellCollectionViewCell.swift
+++
@@ -1,7 +0,0 @@
-//
-//  CourseCellCollectionViewCell.swift
-//  AppGestionUAM
-//
-//  Created by David Sanchez on 15/11/24.
-//
-
---
+++ AppGestionUAM/AppGestionUAM/Views/Courses/Cells/CourseCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CourseCollectionViewCell.swift
+//  AppGestionUAM
+//
+//  Created by David Sanchez on 16/11/24.
+//
+
+import UIKit
+
+class CourseCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
