b301094855af2f1b57c3cf97768406f57f9c9691
David Sánchez <164054263+Djave17@users.noreply.github.com>
2025-02-01 22:43:37 -0600

    Vista de favoritos

---
+++ AppGestionUAM/AppGestionUAM/Views/FavoriteCourses/Cell /FavoriteCoursesCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteCoursesCollectionViewCell.swift
+//  AppGestionUAM
+//
+//  Created by David Sanchez on 1/2/25.
+//
+
+import UIKit
+
+class FavoriteCoursesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
