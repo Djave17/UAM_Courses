ccc415aa604f9c94db80603c6a51c1111d7d888d
David Sánchez <164054263+Djave17@users.noreply.github.com>
2025-02-05 15:17:03 -0600

    Resources

--- AppGestionUAM/AppGestionUAM/Views/Onboarding/ONB3/Onboarding4ViewController.swift
+++ AppGestionUAM/AppGestionUAM/Views/Onboarding/ONB3/Onboarding4ViewController.swift
@@ -85,7 +85,7 @@ class Onboarding4ViewController: UIViewController {
     private func setupVideoPlayer() {
         // Ruta del video en el bundle
         guard let videoPath = Bundle.main.path(forResource: "vd_Onb4", ofType: "mov") else {
-            print("Error: No se encontró el video vd_Onb3.mov en el bundle.")
+            debugPrint("Error: No se encontró el video vd_Onb4.mov en el bundle.")
             return
         }
         
